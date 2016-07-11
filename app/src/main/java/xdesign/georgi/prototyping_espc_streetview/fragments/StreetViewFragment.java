eda032a9eb65e79f582de91c6f23190029bf81fa
= <koemdjiev@gmail.com>
2016-07-11 11:30:33 +0100

    Added all of the files and adapters for the tabbed view to function.

---
+++ app/src/main/java/xdesign/georgi/prototyping_espc_streetview/fragments/StreetViewFragment.java
@@ -0,0 +1,49 @@
+package xdesign.georgi.prototyping_espc_streetview.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import xdesign.georgi.prototyping_espc_streetview.R;
+
+/**
+ * Created by georgi on 11/07/16.
+ */
+public class StreetViewFragment extends Fragment implements OnStreetViewPanoramaReadyCallback {
+    private StreetViewPanoramaFragment streetViewPanoramaFragment;
+//    private GoogleMap mMap;
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        streetViewPanoramaFragment = (StreetViewPanoramaFragment) getActivity().getFragmentManager().findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+    }
+
+    //Overriden method onCreateView
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Returning the layout file after inflating
+        //Change R.layout.tab1 in you classes
+        return inflater.inflate(R.layout.street_virew_fragment_layout, container, false);
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(new LatLng(-33.87365, 151.20689));
+    }
+}
